9d066d92f92ce20b6060bb9155f77aabb903d15a
Andrew <milichab@gmail.com>
2015-07-29 08:37:17 -0400

    Removed threading
    Getting H12 error on Heroku

--- App.py
+++ App.py
@@ -91,4 +91,4 @@ def init():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.debug = False
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port)
