458c62bdb4efc82e1807fe644ce49de5df4a3d17
Andrew <milichab@gmail.com>
2015-07-30 17:50:17 -0400

    Another rename fix

--- FlightFiles.py
+++ FlightFiles.py
@@ -5,7 +5,7 @@ from geopy.distance import vincenty
 from LatLon import LatLon, Latitude, Longitude
 from geomag import mag_heading
 from BeautifulSoup import BeautifulSoup
-from Downloadmap import *
+from downloadmap import *
 from Elevations import *
 import urllib, re, sys, os, math, copy
 import pygmaps 
