972b65680a653e60b44f3f9452050b1bedcdad45
Andrew <milichab@gmail.com>
2016-03-13 19:25:33 -0700

    added telemetry for coloring

---
+++ telemetry.py
@@ -0,0 +1,21 @@
+00:00:05:27.71 msg: Incoming char: 48 : H, state: 0, CMDS = 20 7, LENGTH=16, CKA=1C, CKB=F9, NUM_PACKETS=16
+00:00:05:27.72 msg: Incoming char: 65 : e, state: 1, CMDS = 20 7, LENGTH=16, CKA=0, CKB=0, NUM_PACKETS=0
+00:00:05:27.73 msg: Incoming char: 20 :  , state: 2, CMDS = 20 7, LENGTH=16, CKA=0, CKB=0, NUM_PACKETS=0
+00:00:05:27.74 msg: Incoming char: 4 : [04], state: 3, CMDS = 20 7, LENGTH=16, CKA=20, CKB=20, NUM_PACKETS=0
+00:00:05:27.75 msg: Incoming char: 0 : 
+00:00:05:27.75 msg: Length 1 0
+00:00:05:27.76 msg: Incoming char: 14 : [14], state: 5, CMDS = 20 4, LENGTH=0, CKA=24, CKB=68, NUM_PACKETS=0
+00:00:05:27.77 msg: Length 2 20
+00:00:05:27.77 msg: Incoming char: 38 : 8, state: 6, CMDS = 20 4, LENGTH=20, CKA=38, CKB=A0, NUM_PACKETS=0
+00:00:05:27.78 msg: CK A GOOD
+00:00:05:27.78 msg: Incoming char: A0 :  , state: 7, CMDS = 20 4, LENGTH=20, CKA=38, CKB=A0, NUM_PACKETS=0
+00:00:05:27.79 msg: CK B GOOD
+00:00:05:28.05 msg: Incoming char: 61 : a, state: 8, CMDS = 20 4, LENGTH=20, CKA=E, CKB=62, NUM_PACKETS=13
+00:00:05:28.20 msg: CK A GOOD 
+00:00:05:28.20 msg: Incoming char: E2 : â, state: 9, CMDS = 20 4, LENGTH=20, CKA=24, CKB=E2, NUM_PACKETS=20
+00:00:05:28.21 msg: CK B GOOD 
+00:00:05:28.21 msg: Received a valid packet. 
+00:00:05:28.22 msg: Decoding Packet 
+00:00:05:28.22 msg: Radio Command: 2004
+00:00:05:28.22 msg: Packet decoded as a GS message  
+
